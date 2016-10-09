2befe7f4ba6481d0f57e985e79e4c9c7ceb1c236
Andrea De Rinaldis <andrea@derinaldis.com>
2016-10-09 09:02:25 +0200

    phase 04 partial

--- src/test/java/com/derinaldis/springapp/web/InventoryControllerTest.java
+++ src/test/java/com/derinaldis/springapp/web/InventoryControllerTest.java
@@ -1,17 +1,21 @@
 package com.derinaldis.springapp.web;
 
 import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
 import com.derinaldis.springapp.web.InventoryController;
+import com.derinaldis.springapp.service.SimpleProductManager;
 import junit.framework.TestCase;
 
 public class InventoryControllerTest extends TestCase {
 
 public void testHandleRequestView() throws Exception {
         InventoryController controller = new InventoryController();
+        controller.setProductManager(new SimpleProductManager());
         ModelAndView modelAndView = controller.handleRequest(null, null);
         assertEquals("hello", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel());
-        String nowValue = (String) modelAndView.getModel().get("now");
+        Map modelMap = (Map) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
         assertNotNull(nowValue);
 }
 
